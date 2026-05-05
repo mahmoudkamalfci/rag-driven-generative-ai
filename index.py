472bd739e2f16ae519fbfc44b01e090951abb625
mahmoudkamalfci <mahmoudkamal01099@gmail.com>
2026-05-05 03:27:08 +0300

    updates

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import deeplake
+
+# Initialize the local dataset
+ds = deeplake.empty('./openai_deeplake_db', overwrite=True)
+
+# Create the tensors
+ds.create_tensor('text', htype='text')
+ds.create_tensor('metadata', htype='json')
+ds.create_tensor('embedding', htype='embedding', dtype='float32', sample_compression=None)
